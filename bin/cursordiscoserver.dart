af8be2f8f2215fc843891f9c9c25750c635c0325
Thomas Caserta <tomcaserta@gmail.com>
2013-06-23 17:38:47 +0100

    Uploaded actual code

---
+++ bin/cursordiscoserver.dart
@@ -0,0 +1,124 @@
+library CursorDiscoServer;
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:math';
+
+part 'dj_manager.dart';
+
+int port = 8000;
+
+void main() {
+  DJManager.loadDisco();
+  /// Start a new websocket server on [port]
+  HttpServer.bind('0.0.0.0', port).then((HttpServer server) { 
+    print('Starting CursorDisco on port: $port');
+    try { 
+      server.transform(new WebSocketTransformer()).listen(CursorClientServer.onConnection);
+    }
+    catch (E) {
+      
+    }
+  }, onError: (error) => print("Error starting HTTP server: $error"));
+ 
+}
+
+
+/// Holds all our websocket clients
+class CursorClientServer {
+  static Map<String, CursorClientServer> connections = new Map<String, CursorClientServer>();
+  dynamic ws;
+  int currentPlayback = 0;
+  
+  CursorClientServer (this.ws) { if (!connections.containsKey(this.ws.hashCode.toString())) { connections[this.ws.hashCode.toString()] = this; } }
+  
+  /// Sends a [message] to the client
+  void send (String message) {
+    print("> Sent: ${message}");
+    this.ws.add(message); 
+  }
+   
+  //Static Methods
+  
+  
+  /// Sends a disconnect message to all clients and destroys the connection [conn]
+  static void destroy (conn) {
+    print("User disconnected");
+    CursorClientServer.sendToAll("DISCONNECT ${conn.hashCode}", conn);
+    connections.remove(conn.hashCode.toString());
+  }
+
+  /// Sends a message to all clients with an open WebSocket
+  static void sendToAll (String message, [WebSocket self]) {
+    CursorClientServer.connections.forEach((String hash, CursorClientServer connection) {
+      if (self != null) {
+        if (self.hashCode.toString() != hash) {
+          connection.send(message);
+        }
+      } else connection.send(message);
+    });
+  }
+
+  /// Fetches the instance of CursorClientServer which matches the WebSocket [conn]
+  static CursorClientServer getCCC (WebSocket conn) {
+    return CursorClientServer.connections[conn.hashCode.toString()];
+  }
+  
+  /// Is called every time there is a new connection to the server
+  /// Creates the associated CursorClientServer and sends a message
+  /// to all clients with the hashcode.
+  static void onConnection (conn) {
+    print("New Connection :)");
+    
+    CursorClientServer current = new CursorClientServer(conn);
+    
+    CursorClientServer.sendToAll("NEWCONNECTION ${conn.hashCode}", conn);
+    
+    // Sync client up with the current background and song
+    DJManager.sendCurrBackground(current);
+    DJManager.sendCurrSongAndTime(current);
+    CursorClientServer.connections.forEach((String k,CursorClientServer v) {
+      print("$k == ${conn.hashCode}");
+      if (k != conn.hashCode.toString()) current.send("NEWCONNECTION $k");    
+    });
+    conn.listen((message) {
+      CursorClientServer CCC = CursorClientServer.getCCC(conn);
+      print('< Recieved: $message FROM ID: ${CCC.ws.hashCode}');
+      CursorClientServer.handleResponse(CCC, message);
+    },
+        onDone: () => CursorClientServer.destroy(conn),
+        onError: (e) => CursorClientServer.destroy(conn)
+    );
+  }
+ 
+  /// Response handler for the connected clients
+  static void handleResponse (CursorClientServer self, String message) {
+      var splitMsg = message.split(" ");
+      switch (splitMsg[0]) {
+        case "MOVECURSOR": 
+          // TODO: Add error handling if a malicious user sends something bad
+          double x = double.parse(splitMsg[1]);
+          double y = double.parse(splitMsg[2]);
+          
+          // Convert the X Y co-ordinates sent by the client into a percentage of the screen
+          double screen_width = double.parse(splitMsg[3]);
+          double screen_height = double.parse(splitMsg[4]);
+         
+          double percent_width = (x /screen_width) * 100;
+          double percent_height = (y / screen_height) * 100;
+          
+          // Send cursor update to all clients
+          CursorClientServer.sendToAll("MOVECURSOR ${self.ws.hashCode} $percent_width $percent_height",self.ws);
+          break;
+        case  "TIMEUPD": 
+          // This message is sent every time a clients music progresses. 
+          int songTime = (double.parse(splitMsg[1])).toInt();
+          int songSync = (songTime - (DJManager.songTime.elapsedMilliseconds / 1000)).toInt();
+          
+          // If the music is out of sync then we should resend the song and time
+          if (!(songSync < 5 && songSync > -5)) DJManager.sendCurrSongAndTime(self); 
+         break;
+      }
+      
+  } 
+}
---
+++ bin/dj_manager.dart
@@ -0,0 +1,78 @@
+part of CursorDiscoServer;
+
+
+/// Why a static class you wonder, isnt that against the guidelines?! 
+/// Technically yes but it didnt make sense NOT to have a class for this
+/// It makes it much easier to see where the functions are going
+/// and what the functions are doing because of the class name
+class DJManager {
+  static String color = "lightblue";
+  //Long list of colors coming up!
+  static List<String> colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Green","GreenYellow","HoneyDew","HotPink","IndianRed ","Indigo ","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+  static int sincelastbg = 0;
+  static String currentSong;
+  static Map<String, int> songs = new Map<String, int>();
+  static Stopwatch timeGoing = new Stopwatch()..start();
+  static Stopwatch songTime = new Stopwatch()..start();
+  
+  /// Called to load up our disco with songs
+  static void loadDisco () {
+    // Fill our songs map with the song name and length in seconds.
+    songs["justcantgetenough.mp3"] = 227; 
+    songs["funkytown.mp3"] = 344;
+    songs["Stringed_Disco.mp3"] = 174;
+    currentSong = DJManager.getRandomSong();
+    new Timer.periodic (new Duration(milliseconds: 10), (t) { DJManager.tick(); });
+    
+  }
+  
+  /// Used to resync clients. Sends the current song and time.
+  static void sendCurrSongAndTime (CursorClientServer cur) {
+    cur.send("CHANGESONG ${currentSong} ${(songTime.elapsedMilliseconds / 1000)}");
+  }
+  
+  /// Returns a random song file name
+  static String getRandomSong () {
+    Random r = new Random();
+    int n = r.nextInt(songs.length);
+    Iterable key = songs.keys;
+    return key.elementAt(n);
+  }
+  
+  /// Grabs a random song and sends it to the clients.
+  static void changeSong () {
+    String rkey = DJManager.getRandomSong();
+    songTime.reset();    
+    currentSong = rkey;
+    CursorClientServer.sendToAll("CHANGESONG ${rkey} 0");
+  }
+  
+  /// Used to sync clients up. Sends the current background.
+  static void sendCurrBackground (CursorClientServer cur) {
+    cur.send("CHANGEBG ${color}");
+  }
+  
+  /// Changes the background (Randomly) and sends an update to all clients
+  static void changeBackground () {
+    Random r = new Random();
+    int n = r.nextInt(colors.length);
+    color = colors[n];
+    CursorClientServer.sendToAll("CHANGEBG ${color}");
+  }
+  
+  /// Called every 10 ms
+  static void tick () {
+    if ((songTime.elapsedMilliseconds / 1000).toInt() > songs[currentSong]) {
+      DJManager.changeSong();
+      songTime.reset();
+    }
+    if (sincelastbg > 500) {
+       DJManager.changeBackground();
+       sincelastbg = 0;
+      
+    }
+    sincelastbg += timeGoing.elapsedMilliseconds;
+    timeGoing.reset();
+  }
+}
